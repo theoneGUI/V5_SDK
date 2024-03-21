1c4174bf79f235a3220f56a17428c878d7105993
unknown <49991562+theoneGUI@users.noreply.github.com>
2024-03-20 20:36:46 -0500

    Starting code

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include "../include/vex.h" // Get all of our Vex objects (like the brain, controller, motors, etc)
+
+using namespace vex;
+
+void usercontrol(void) {
+  // User control code here, inside the loop
+  while (1) {
+    // This is the main execution loop for the user control program.
+    // Each time through the loop your program should update motor + servo
+    // values based on feedback from the joysticks.
+
+    // ........................................................................
+    // Insert user code here. This is where you use the joystick values to
+    // update your motors, etc.
+    // ........................................................................
+
+    wait(20, msec); // Sleep the task for a short amount of time to
+                    // prevent wasted resources.
+  }
+}
+
+int main() {
+    usercontrol(); // when starting our code, run user control
+}
